d741a322768b64139203965fc6a55b9f7c2b37cf
Ali Abbani <aliabbani08@gmail.com>
2023-03-27 19:56:56 +0100

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -22,7 +23,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer">
-            Ali Abbani{" "}
+            Ali Abbani
             <span className="sm:block hidden">| Full Stack Developer</span>
           </p>
         </Link>
